02bd5679d89c2f15f679bee66ede920defd81a24
L00143846 <L00143846@student.lyit.ie>
2017-11-23 15:38:22 +0000

    alt code 23/11 @ 15:30

--- EquipMaintSys1/VScode/EquipMaintSys1/MainWindow.xaml.cs
+++ EquipMaintSys1/VScode/EquipMaintSys1/MainWindow.xaml.cs
@@ -39,7 +39,7 @@ namespace EquipMaintSys1
         int fauTbx = 0;
         //admin tab variables
         int admB = 0;
-        string admY = "z";
+        //string admY = "z";
         int admMachineSelected = 0;
         #endregion -- VARIABLES
         public MainWindow()
@@ -51,103 +51,9 @@ namespace EquipMaintSys1
             faultsBtn.Visibility = System.Windows.Visibility.Hidden;
             tbxFaultDetail.Visibility = System.Windows.Visibility.Hidden;
         }
-        #region REGION -- EXIT TAB
-        private void exitBtn_Click(object sender, RoutedEventArgs e)
-        {
-            Close();
-            Environment.Exit(0);
-        }
-        #endregion REGION -- EXIT TAB
-        #region REGION -- ADMIN TAB
-        #region REGION -- adminTabBtnSecection
-        private void queryBreakBtn_Click(object sender, RoutedEventArgs e)
-        {
-            admB = 1;
-            cbo_Selection.Visibility = System.Windows.Visibility.Visible;
-        }
-        private void queryMaintBtn_Click(object sender, RoutedEventArgs e)
-        {
-            admB = 2;
-            cbo_Selection.Visibility = System.Windows.Visibility.Visible;
-        }
-        private void queryCompBtn_Click(object sender, RoutedEventArgs e)
-        {
-            admB = 3;
-            cbo_Selection.Visibility = System.Windows.Visibility.Visible;
-        }
-        private void queryUsersBtn_Click(object sender, RoutedEventArgs e)
-        {
-            admB = 4;
-            cbo_Selection.Visibility = System.Windows.Visibility.Visible;
-        }
-        public void addBtn_Click(object sender, RoutedEventArgs e)
-        {
-            admB = 5;
-            cbo_Selection.Visibility = System.Windows.Visibility.Visible;
 
-
-            fauB = 1;
-            cbo_Fault.Visibility = System.Windows.Visibility.Visible;
-
-            try
-            {
-                con.Open();
-                //SqlCommand commObject1 = new SqlCommand("select Name from Equiptment", con);
-                SqlDataAdapter commObject1 = new SqlDataAdapter("select * from Item", con);
-                DataTable dt = new DataTable();
-                commObject1.Fill(dt);
-                foreach (DataRow row in dt.Rows)
-                {
-                    cbo_Selection.Items.Add(row["Name"].ToString());
-                    //cbo_Fault.Items.Add(row["Name"].ToString());
-                    //listV_fault_data.Items.Add(row["Name"].ToString());
-                }
-            }// end try
-
-            catch (Exception ex)
-            { MessageBox.Show(ex.Message); }// end catch
-
-            finally
-            { con.Close(); }//end finally
-
-            // L00143846_EquiptMaintSys1Entities db = new L00143846_EquiptMaintSys1Entities();
-            // using (L00143846_EquiptMaintSys1Entities db = new L00143846_EquiptMaintSys1Entities())
-            //con.Open();
-            //SqlCommand commObject1 = new SqlCommand("select Name from Item", con);
-
-            // L00143846_EquiptMaintSys1Entities db = new L00143846_EquiptMaintSys1Entities();
-            // using (L00143846_EquiptMaintSys1Entities db = new L00143846_EquiptMaintSys1Entities())
-            //try
-            //{
-            //    //SqlDataAdapter sda = new SqlDataAdapter("select Name from Item", con);
-            //    SqlDataReader dr = commObject1.ExecuteReader();
-            //    while (dr.Read())
-            //    {
-            //        //ListViewItem y = new ListViewItem(dr["Names"].ToString());
-            //        //tblkOutput.ToString();
-            //        //lbxOutput.ToString();
-            //    }
-            //    //DataTable dt = new DataTable();
-            //    //sda.Fill(dt);
-            //    //cbo_Selection.Items.Add(dt);
-            //}// end try
-
-
-        }
-        private void archiveBtn_Click(object sender, RoutedEventArgs e)
-        {
-            admB = 6;
-        }
-        #endregion  REGION -- adminTabBtnSecection
-        private void cbo_Selection_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            adminBtn.Visibility = System.Windows.Visibility.Visible;
-            admMachineSelected = 1;
-        }
-
-
-
-        #endregion REGION -- ADMIN TAB
+        #region REGION -- Search TAB
+        #endregion REGION -- Search TAB
         #region REGION -- FAULT TAB
         private void Fault_Tab_Initialized(object sender, EventArgs e)
         {
@@ -263,6 +169,129 @@ namespace EquipMaintSys1
             #endregion REGION -- resolve a fault
         }// end faultsBtn_Click
         #endregion FAULT TAB
+        #region REGION -- ADMIN TAB
+        #region REGION -- adminTabBtnSecection
+        private void queryBreakBtn_Click(object sender, RoutedEventArgs e)
+        {
+            admB = 1;
+            cbo_Selection.Visibility = System.Windows.Visibility.Visible;
+        }
+        private void queryMaintBtn_Click(object sender, RoutedEventArgs e)
+        {
+            admB = 2;
+            cbo_Selection.Visibility = System.Windows.Visibility.Visible;
+        }
+        private void queryCompBtn_Click(object sender, RoutedEventArgs e)
+        {
+            admB = 3;
+            cbo_Selection.Visibility = System.Windows.Visibility.Visible;
+        }
+        private void queryUsersBtn_Click(object sender, RoutedEventArgs e)
+        {
+            admB = 4;
+            cbo_Selection.Visibility = System.Windows.Visibility.Visible;
+        }
+        public void addBtn_Click(object sender, RoutedEventArgs e)
+        {
+            admB = 5;
+            cbo_Selection.Visibility = System.Windows.Visibility.Visible;
+
+
+            fauB = 1;
+            cbo_Fault.Visibility = System.Windows.Visibility.Visible;
+
+            try
+            {
+                con.Open();
+                //SqlCommand commObject1 = new SqlCommand("select Name from Equiptment", con);
+                SqlDataAdapter commObject1 = new SqlDataAdapter("select * from Item", con);
+                DataTable dt = new DataTable();
+                commObject1.Fill(dt);
+                foreach (DataRow row in dt.Rows)
+                {
+                    cbo_Selection.Items.Add(row["Name"].ToString());
+                    //cbo_Fault.Items.Add(row["Name"].ToString());
+                    //listV_fault_data.Items.Add(row["Name"].ToString());
+                }
+            }// end try
+
+            catch (Exception ex)
+            { MessageBox.Show(ex.Message); }// end catch
+
+            finally
+            { con.Close(); }//end finally
+
+            // L00143846_EquiptMaintSys1Entities db = new L00143846_EquiptMaintSys1Entities();
+            // using (L00143846_EquiptMaintSys1Entities db = new L00143846_EquiptMaintSys1Entities())
+            //con.Open();
+            //SqlCommand commObject1 = new SqlCommand("select Name from Item", con);
+
+            // L00143846_EquiptMaintSys1Entities db = new L00143846_EquiptMaintSys1Entities();
+            // using (L00143846_EquiptMaintSys1Entities db = new L00143846_EquiptMaintSys1Entities())
+            //try
+            //{
+            //    //SqlDataAdapter sda = new SqlDataAdapter("select Name from Item", con);
+            //    SqlDataReader dr = commObject1.ExecuteReader();
+            //    while (dr.Read())
+            //    {
+            //        //ListViewItem y = new ListViewItem(dr["Names"].ToString());
+            //        //tblkOutput.ToString();
+            //        //lbxOutput.ToString();
+            //    }
+            //    //DataTable dt = new DataTable();
+            //    //sda.Fill(dt);
+            //    //cbo_Selection.Items.Add(dt);
+            //}// end try
+
+
+        }
+        private void archiveBtn_Click(object sender, RoutedEventArgs e)
+        {
+            admB = 6;
+            cbo_Selection.Visibility = System.Windows.Visibility.Visible;
+        }
+        #endregion  REGION -- adminTabBtnSecection
+        private void cbo_Selection_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            adminBtn.Visibility = System.Windows.Visibility.Visible;
+            admMachineSelected = 1;
+        }
+        private void adminBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if(admB == 1 && admMachineSelected == 1) { }
+            else if (admB == 2 && admMachineSelected == 1)
+            {
+                var adminGridView = new GridView();
+                this.listV_data.View = adminGridView();
+                adminGridView.Columns.Add(new GridViewColumn { Header = "Name", DisplayMemberBinding = new Binding("Name") });
+                adminGridView.Columns.Add(new GridViewColumn { Header = "Equiptment", DisplayMemberBinding = new Binding("Equiptment") });
+                adminGridView.Columns.Add(new GridViewColumn { Header = "Downtime", DisplayMemberBinding = new Binding("Downtime") });
+                adminGridView.Columns.Add(new GridViewColumn { Header = "Implementor", DisplayMemberBinding = new Binding("Implementor") });
+                adminGridView.Columns.Add(new GridViewColumn { Header = "Job_Description", DisplayMemberBinding = new Binding("Job_Description") });
+                adminGridView.Columns.Add(new GridViewColumn { Header = "Frequency", DisplayMemberBinding = new Binding("Frequency") });
+                adminGridView.Columns.Add(new GridViewColumn { Header = "Start_Date_Time", DisplayMemberBinding = new Binding("Start_Date_Time") });
+                adminGridView.Columns.Add(new GridViewColumn { Header = "End_Date_Time", DisplayMemberBinding = new Binding("End_Date_Time") });
+                adminGridView.Columns.Add(new GridViewColumn { Header = "Archived", DisplayMemberBinding = new Binding("Archived") });
+                adminGridView.Columns.Add(new GridViewColumn { Header = "Archived_by", DisplayMemberBinding = new Binding("Archived_by") });
+
+            }
+            else if (admB == 3 && admMachineSelected == 1) { }
+            else if (admB == 4 && admMachineSelected == 1) { }
+            else if (admB == 5 && admMachineSelected == 1) { }
+            else if (admB == 6 && admMachineSelected == 1) { }
+        }
+
+
+        #endregion REGION -- ADMIN TAB
+        #region REGION -- EXIT TAB
+        private void exitBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+            Environment.Exit(0);
+        }
+        #endregion REGION -- EXIT TAB
+
+
     }// end main
 }// end namespace
 
